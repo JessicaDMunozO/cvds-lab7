3c8c047fce886dcc6407cff97fd7036318ca468f
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-04-11 16:05:38 -0500

    Conexión establecida con la base de datos

---
+++ spring-boot-jpa-medium-demo/src/main/java/com/cvds/lab7/Employee.java
@@ -0,0 +1,125 @@
+package com.cvds.lab7;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long employeeId;
+	private String firstName;
+	private String lastName;
+	private String role;
+	private Double salary;
+
+	public Employee() {
+
+	}
+
+	public Employee(String string, String string2, String string3, double d) {
+		this.firstName = string;
+		this.lastName = string2;
+		this.role = string3;
+		this.salary = d;
+	}
+
+	public Long getEmployeeId() {
+		return this.employeeId;
+	}
+
+	public void setEmployeeId(Long EmployeeId) {
+		this.employeeId = EmployeeId;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String FirstName) {
+		this.firstName = FirstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String LastName) {
+		this.lastName = LastName;
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String Role) {
+		this.role = Role;
+	}
+
+	public Double getSalary() {
+		return this.salary;
+	}
+
+	public void setSalary(Double Salary) {
+		this.salary = Salary;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((salary == null) ? 0 : salary.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (employeeId == null) {
+			if (other.employeeId != null)
+				return false;
+		} else if (!employeeId.equals(other.employeeId))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (salary == null) {
+			if (other.salary != null)
+				return false;
+		} else if (!salary.equals(other.salary))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", role="
+				+ role + ", salary=" + salary + "]";
+	}
+
+}
